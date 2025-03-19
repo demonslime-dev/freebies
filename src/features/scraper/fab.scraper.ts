c4e708531bbc6052960619075c3cca34747bf278
demonslime <122673151+demonslime-dev@users.noreply.github.com>
2025-03-19 20:39:01 +0530

    Fix fab scraper

--- src/features/scraper/fab.scraper.ts
+++ src/features/scraper/fab.scraper.ts
@@ -1,53 +1,66 @@
 import { createBrowserContext } from "@/common/browser.js";
-import logger from '@/common/logger.js';
-import { Prisma } from '@prisma/client';
-import { convertTo24HourFormat } from '@scraper/utils.scraper.js';
+import logger from "@/common/logger.js";
+import { Prisma } from "@prisma/client";
+import { DateTime } from "luxon";
 
 export async function getFreeAssetsFromFab(): Promise<Prisma.ProductCreateInput[]> {
-    const assetsUrl = 'https://www.fab.com';
-    const context = await createBrowserContext();
-    try {
-        logger.info(`Getting free products from ${assetsUrl}`);
-        const page = await context.newPage();
-        await page.goto(assetsUrl);
-        await page.getByRole('link', {name: 'Limited-Time Free'}).click();
-        await page.waitForTimeout(5000);
+  const assetsUrl = "https://www.fab.com";
+  const context = await createBrowserContext();
+  try {
+    logger.info(`Getting free products from ${assetsUrl}`);
+    const page = await context.newPage();
+    await page.goto(assetsUrl);
+    await page.getByRole("link", { name: "Limited-Time Free", exact: true }).click();
+    await page.waitForTimeout(5000);
 
-        const saleEndDateText = await page.getByText('Limited-Time Free').innerText();
-        const [_ , month, day, hours, period, zone] = saleEndDateText.match(/(\d+)\/(\d+)\s(\d+)(AM|PM)\s(\w+)/)!;
-        const datetimeString = `${new Date().getUTCFullYear()}-${month.padStart(2, '0')}-${day.padStart(2, '0')}T${convertTo24HourFormat(hours, '0', period)}:00-05:00`;
+    const saleEndDateText = await page.getByText("Limited-Time Free").innerText();
+    const dateTimeIsoStr = getDateFromString(saleEndDateText);
 
-        const titleLocators = await page.locator('a.fabkit-Typography-root > div').all();
-        const thumbnailLocators = await page.locator('.fabkit-Thumbnail-root > img').all();
-        logger.info(`${thumbnailLocators.length} free products found`);
+    const titleLocators = await page.locator("a.fabkit-Typography-root > div").all();
+    const thumbnailLocators = await page.locator(".fabkit-Thumbnail-root > img").all();
+    logger.info(`${thumbnailLocators.length} free products found`);
 
-        const products: Prisma.ProductCreateInput[] = [];
-        for (let i = 0; i < thumbnailLocators.length; i++) {
-            const title = await titleLocators[i].innerText();
-            let url = await titleLocators[i].locator('..').getAttribute('href');
+    const products: Prisma.ProductCreateInput[] = [];
+    for (let i = 0; i < thumbnailLocators.length; i++) {
+      const title = await titleLocators[i].innerText();
+      let url = await titleLocators[i].locator("..").getAttribute("href");
 
-            if (!url) {
-                logger.error("Unable to retrieve product url");
-                continue;
-            }
+      if (!url) {
+        logger.error("Unable to retrieve product url");
+        continue;
+      }
 
-            url = `https://www.fab.com${url}`;
-            const imageUrl = await thumbnailLocators[i].getAttribute('src');
+      url = `https://www.fab.com${url}`;
+      const imageUrl = await thumbnailLocators[i].getAttribute("src");
 
-            if (!imageUrl) {
-                logger.error("Unable to retrieve product url");
-                continue;
-            }
+      if (!imageUrl) {
+        logger.error("Unable to retrieve product url");
+        continue;
+      }
 
-            products.push({
-                title,
-                url,
-                images: [imageUrl],
-                saleEndDate: new Date(datetimeString)
-            });
-        }
+      products.push({
+        title,
+        url,
+        images: [imageUrl],
+        saleEndDate: new Date(dateTimeIsoStr),
+      });
+    }
 
-        logger.info(`${products.length} free products retrieved`);
-        return products;
-    } finally { await context.browser()?.close(); }
+    logger.info(`${products.length} free products retrieved`);
+    return products;
+  } finally {
+    await context.browser()?.close();
+  }
+}
+
+function getDateFromString(dateTimeString: string) {
+  const match = dateTimeString.match(/(\w+)\s(\d+)\w+\s(\d+):(\d+)\s(AM|PM)\s(\w+)/);
+  if (!match) throw new Error("Invalid date string");
+  const [_, month, day, hours, minutes, period, zone] = match;
+  const year = new Date().getUTCFullYear();
+  const formattedDateTimeStr = `${year} ${month} ${day} ${hours}:${minutes} ${period}`;
+  const dateTime = DateTime.fromFormat(formattedDateTimeStr, "yyyy MMM d t").setZone("America/New_York");
+  const dateTimeIsoStr = dateTime.toUTC().toISO();
+  if (!dateTimeIsoStr) throw Error("Invalid date string");
+  return dateTimeIsoStr;
 }
