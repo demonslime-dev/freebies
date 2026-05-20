2a59b3eae3889e0c83a54a066c902d1e515232fe
demonslime <122673151+demonslime-dev@users.noreply.github.com>
2026-05-20 20:55:54 +0530

    Switch drizzle import from node-postgres to neon-http

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,5 @@
-// import { drizzle } from 'drizzle-orm/neon-http';
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+// import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "./env.ts";
 import { relations } from "./relations.ts";
 import * as schema from "./schema.ts";
